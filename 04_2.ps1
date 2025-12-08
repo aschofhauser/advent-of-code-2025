b4218161efd0488ab18d8d3f369fcc0d01e637f8
Alexandra Schofhauser <93743353+aschofhauser@users.noreply.github.com>
2025-12-08 10:31:19 +0100

    Add day 4 part 2 solution

--- 04_2.ps1
+++ 04_2.ps1
@@ -129,3 +129,56 @@ Stop once no more rolls of paper are accessible by a forklift. In this example, 
 
 Start with your original diagram. How many rolls of paper in total can be removed by the Elves and their forklifts?
 #>
+
+param (
+    # Filepath containing the battery joltage ratings
+    [Parameter(Mandatory)]
+    [string]$Path
+)
+
+$Diagram = Get-Content -Path $Path
+
+$TotalAccessibleRollCount = 0
+
+do {
+    $CurrentAccessibleRollCount = 0
+    for ($y = 0; $y -lt $Diagram.Count; $y++) {
+        for ($x = 0; $x -lt $Diagram[$y].Length; $x++) {
+            $AdjacentRollCount = 0
+            if ($Diagram[$y][$x] -eq '@') {
+                for ($i = [math]::Max($y-1, 0); $i -le [math]::Min($y+1, $Diagram.Count-1); $i++) {
+                    for ($j = [math]::Max($x-1, 0); $j -le [math]::Min($x+1, $Diagram[$y].Length-1); $j++) {
+                        if ($Diagram[$i][$j] -eq '@' -or $Diagram[$i][$j] -eq 'x') {
+                            $AdjacentRollCount++
+                        }
+                    }
+                }
+            }
+            if ($AdjacentRollCount -eq 0) {
+                #Write-Host "." -NoNewline
+            }
+            elseif ($AdjacentRollCount -le 4) {
+                $CurrentAccessibleRollCount++
+                $Diagram[$y] = $Diagram[$y].Remove($x,1).Insert($x,'x')
+                #Write-Host "x" -NoNewline
+            }
+            else {
+                #Write-Host '@' -NoNewline
+            }
+        }
+        #Write-Host ''
+    }
+    $TotalAccessibleRollCount += $CurrentAccessibleRollCount
+    Write-Output "$($CurrentAccessibleRollCount) rolls of paper can be removed"
+    # Remove rolls
+    for ($y = 0; $y -lt $Diagram.Count; $y++) {
+        for ($x = 0; $x -lt $Diagram[$y].Length; $x++) {
+            if ($Diagram[$y][$x] -eq 'x') {
+                $Diagram[$y] = $Diagram[$y].Remove($x,1).Insert($x,'.')
+            }
+        }
+    }
+}
+while ($CurrentAccessibleRollCount -gt 0)
+
+Write-Output "$($TotalAccessibleRollCount) rolls of paper can be removed in total"
